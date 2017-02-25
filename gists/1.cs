6cc98d705e44f4caf74128a7737dd57543b72863
ewoutnwn <ewout@linguistadores.com>
2017-02-25 19:07:13 +0100

    Gists created separately

--- gists/1.cs
+++
@@ -1,7 +0,0 @@
-public Subdomain1DbContext: DbContext
-{
-	public DbSet<DomainObject1> DomainObjects1 { get; set; }
-	public DbSet<DomainObject2> DomainObjects2 { get; set; }
-	:
-	:
-}
--- gists/2.cs
+++
@@ -1,9 +0,0 @@
-var type = dbContext.GetType();
-var properties = type.GetProperties();
-foreach (var property in properties)
-{
-	if (property.PropertyType == typeof (DbSet<T>))
-	{
-		return true;
-	}
-}
