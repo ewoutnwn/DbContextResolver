955a6ff665acc1dc4c0fd04c7dab01998c925fbe
ewoutnwn <ewoutnwn@gmail.com>
2017-02-25 15:44:22 +0100

    Create DbContext2.cs

---
+++ DbContext2.cs
@@ -0,0 +1,26 @@
+namespace Template.Data
+{
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity;
+    using System.Data.Entity.Infrastructure.Annotations;
+    using Domain.Configuration;
+
+    public class DbContext2 : DbContext, IProjectsDbContext
+    {
+        public DbSet<DomainObject3> DomainObjects3 { get; set; }
+        public DbSet<DomainObject4> DomainObjects4 { get; set; }
+
+        public DbContext1() : base("DbConnectionName")
+        {
+        }
+        
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("subdomain1");
+            :
+            :
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
